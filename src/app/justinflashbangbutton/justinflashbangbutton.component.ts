b5f6d4804ead1f210034351e28829e55eb0922cd
Michael Busch <b.michael.2002@web.de>
2022-10-06 21:27:40 +0200

    iguagdgawdaw

---
+++ src/app/justinflashbangbutton/justinflashbangbutton.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-justinflashbangbutton',
+  templateUrl: './justinflashbangbutton.component.html',
+  styleUrls: ['./justinflashbangbutton.component.scss']
+})
+export class JustinflashbangbuttonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
